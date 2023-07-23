005626656e880d81c891d61a4df52296ccee9a54
Chan Nyein Tun <chanyeintun@gmail.com>
2023-07-23 15:12:25 +0630

    fix: permission issue

--- app/index.js
+++ app/index.js
@@ -4,8 +4,6 @@ import { useEffect, useState } from 'react';
 import * as SecureStore from "expo-secure-store";
 import { useDispatch, useSelector } from 'react-redux';
 import { setUsername } from '../store/slices/appSlice';
-import { Camera } from 'expo-camera';
-import * as MediaLibrary from 'expo-media-library';
 import { auth } from '../firebaseConfig';
 import { setAuth } from '../store/slices/appSlice';
 
@@ -14,22 +12,18 @@ export default function App() {
   const authenticated = useSelector(state => state.app.auth);
   const [mounted, setMounted] = useState(false);
 
-
-  const [permission, requestPermission] = Camera.useCameraPermissions();
-  const [permissionM, requestPermissionM] = MediaLibrary.usePermissions();
-
   useEffect(() => {
     checkUsername();
     setMounted(true);
     auth.onAuthStateChanged((user) => {
       dispatch(setAuth(!!user));
     })
-
   }, [])
 
   async function checkUsername() {
     const username = await SecureStore.getItemAsync('username');
     dispatch(setUsername(username ?? ''));
+    console.log('username', username)
   }
 
   useEffect(() => {
@@ -40,19 +34,6 @@ export default function App() {
       router.replace('/login');
     }
   }, [authenticated, mounted])
-
-  useEffect(() => {
-    (async () => {
-      if (permission && permissionM) {
-        if (!permission.granted) {
-          await requestPermission();
-        }
-        if (!permissionM.granted) {
-          requestPermissionM();
-        }
-      }
-    })();
-  }, [permission, permissionM])
 
   return <View />
 }
--- components/Login.js
+++ components/Login.js
@@ -1,6 +1,6 @@
 import { View, StyleSheet, TextInput, Text, ActivityIndicator } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { setAuth, setUsername as setUser } from "../store/slices/appSlice";
 import { Button } from "./Button";
 import { router } from "expo-router";
@@ -10,13 +10,38 @@ import { signInAnonymously } from "firebase/auth/react-native";
 import { auth } from "../firebaseConfig";
 import { showToast } from "../utils";
 import { colors } from "../constants";
+import { Camera } from 'expo-camera';
+import * as MediaLibrary from 'expo-media-library';
 
 export default function Login() {
     const dispatch = useDispatch();
     const user = useSelector(state => state.app.username);
-
+    console.log('User is', user)
     const [username, setUserName] = useState(user);
     const [loading, setLoading] = useState(false);
+    const [permission, requestPermission] = Camera.useCameraPermissions();
+    const [permissionM, requestPermissionM] = MediaLibrary.usePermissions();
+
+    useEffect(() => {
+        if (user) {
+            setUserName(user);
+        }
+    }, [user])
+
+    useEffect(() => {
+        (async () => {
+            if (permission) {
+                if (!permission.granted) {
+                    await requestPermission();
+                }
+            }
+            if (permissionM) {
+                if (!permissionM.granted) {
+                    requestPermissionM();
+                }
+            }
+        })();
+    }, [permission, permissionM])
 
     const submit = async () => {
         setLoading(true);
